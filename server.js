0c2f08444e8d213bf69364df9593de7e4ee9c606
ravi kumar <marveric47@gmail.com>
2018-03-21 01:43:05 +0530

    test

--- server.js
+++ server.js
@@ -5,17 +5,8 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(function (req, res, next){
-  if (req.headers['x-forwarded-proto'] === 'https') {
-    res.redirect('http://' + req.hostname + req.url);
-    }
-    else {
-    next();
-    }
-});
-
 // app.use(express.static('public'));
-app.use(express.static(path.join(__dirname, '../Client/dist')));
+app.use(express.static(path.join(__dirname, './dist')));
 
 app.listen(PORT, function () {
   console.log('Express server is up on port '+PORT);
